28e0ff8360afe56f863c9cb3123651d619c0da57
Lemonkiwi7 <xmagiccandy1@gmail.com>
2019-01-10 16:12:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pathomporn/edittextinput/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.pathomporn.edittextinput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // button
+        final Button btn1 = (Button) findViewById(R.id.btnOK);
+
+    // textview
+        final TextView textV2 = (TextView) findViewById(R.id.txtV2);
+
+    // input text
+        final TextView editText1 = (TextView) findViewById(R.id.edtMsg);
+
+    // perform action on click
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strTxt = editText1.getText().toString();
+                textV2.setText("Your msg = " + strTxt);
+            }
+        });
+    }
+}
